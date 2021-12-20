10e5cea6ee349747f9ef691f93e14022239ea185
Xiaopeng Xu <charlesxu90@gmail.com>
2021-12-20 23:48:40 +0300

    add shell scripts

--- run_training.sh
+++ run_training.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+
 # CharRNN
-# python charRNN/train.py --train_data /home/xiaopeng/Desktop/drug_design/sgpt/data/moses/train.csv --valid_data /home/xiaopeng/Desktop/drug_design/sgpt/data/moses/test.csv --output_dir ./moses/30-epoch --eval --n_epochs 30
+# python charRNN/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/charRNN/30_epochs/ --eval --n_epochs 30
 
 # MCMG
-# python MCMG/data_structs.py /home/xiaopeng/Desktop/drug_design/sgpt/data/moses/train.csv
+python MCMG/data_structs.py --input_file data/moses/train.csv --output_dir data/moses/MCMG/
+# python MCMG/train_prior.py --train-data data/moses/train.csv --valid-data data/moses/test.csv --voc_path data/moses/MCMG/Voc_RE --output_dir result/MCMG/30_epochs/ --num_epochs 30
 
 # MolGPT
+# python molgpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/molgpt/30_epochs/ --num_props 0 --max_epochs 30 --eval
 
 # GPT
-python gpt/train.py --train_data /home/xiaopeng/Desktop/drug_design/sgpt/data/moses/train.csv --valid_data /home/xiaopeng/Desktop/drug_design/sgpt/data/moses/test.csv --output_dir ./moses/gpt-30-epoch/ --eval --n_epochs 30
+# python gpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/gpt/30_epochs/ --eval --n_epochs 30
+
+# SGPT
+# python sgpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/sgpt/30_epochs/ --eval --n_epochs 30
--- test_run.sh
+++ test_run.sh
@@ -5,10 +5,13 @@
 
 # MCMG
 # python MCMG/data_structs.py --input_file data/moses/data_10k.csv --output_dir data/moses/MCMG/
-python MCMG/train_prior.py --train-data data/moses/data_10k.csv --valid-data data/moses/data_10k.csv --voc_path data/moses/MCMG/Voc_RE --output_dir result/MCMG/test_10k/ --num_epochs 1
+# python MCMG/train_prior.py --train-data data/moses/data_10k.csv --valid-data data/moses/data_10k.csv --voc_path data/moses/MCMG/Voc_RE --output_dir result/MCMG/test_10k/ --num_epochs 1
 
 # MolGPT
 # python molgpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/molgpt/test_10k/ --num_props 0 --max_epochs 30 --eval
 
 # GPT
 # python gpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/gpt/test_10k/ --eval --n_epochs 30
+
+# SGPT
+# python sgpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/charRNN/test_10k/ --eval --n_epochs 30

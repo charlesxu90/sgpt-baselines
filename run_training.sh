8011b97d17529d64684d7579a2213b85702dd84a
Xiaopeng Xu <charlesxu90@gmail.com>
2022-01-04 20:54:45 +0300

    tested with gpt

--- run_training.sh
+++ run_training.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-
-# CharRNN
-# python charRNN/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/charRNN/30_epochs/ --eval --n_epochs 30
+# SmilesRNN
+# python SmilesRNN/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/SmilesRNN/80_epochs/ --eval --n_epochs 80
 
 # MCMG
 # python MCMG/data_structs.py --input_file data/moses/train.csv --output_dir data/moses/MCMG/
-# python MCMG/train_prior.py --train-data data/moses/train.csv --valid-data data/moses/test.csv --voc_path data/moses/MCMG/Voc_RE --output_dir result/MCMG/30_epochs/ --num_epochs 30
+# python MCMG/train_prior.py --train-data data/moses/train.csv --valid-data data/moses/test.csv --voc_path data/moses/MCMG/Voc_RE --output_dir result/MCMG/10_epochs/ --num_epochs 10
 
 # MolGPT
-# python molgpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/molgpt/30_epochs/ --num_props 0 --max_epochs 30 --eval
-
-# GPT
-# python gpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/gpt/30_epochs/ --eval --n_epochs 30
+# python molgpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/molgpt/10_epochs/ --num_props 0 --max_epochs 10 --eval
 
 # SGPT
-python sgpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/sgpt/30_epochs/ --eval --n_epochs 30
+# python sgpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/sgpt/10_epochs/ --eval --n_epochs 10
 
-# SmilesRNN
-python SmilesRNN/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/SmilesRNN/30_epochs/ --eval --n_epochs 30
+# GPT
+# python gpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/gpt/20_epochs_16h/ --eval --n_epochs 20 --n_embd 256 --n_head 16 --n_layers 8 --batch_size 512
+# python gpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/gpt/20_epochs_8h/  --eval --n_epochs 20 --n_embd 256 --n_head 8 --n_layers 8 --batch_size 512
+# python gpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/gpt/20_epochs_4h/ --eval --n_epochs 20 --n_embd 256 --n_head 4 --n_layers 8 --batch_size 512
+# python gpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/gpt/20_epochs_16h_1024b/ --eval --n_epochs 20 --n_embd 256 --n_head 16 --n_layers 8 --batch_size 1024
+# python gpt/train.py --train_data data/moses/train.csv --valid_data data/moses/test.csv --output_dir result/gpt/20_epochs_4h_1024b/ --eval --n_epochs 20 --n_embd 256 --n_head 4 --n_layers 8 --batch_size 1024
--- test_run.sh
+++ test_run.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# CharRNN
-# python charRNN/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/charRNN/test_10k/ --eval --n_epochs 30
+# SmilesRNN
+# python SmilesRNN/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/SmilesRNN/test_10k/ --eval --n_epochs 10 --num_to_sample 1000
+
+# MolGPT
+# python molgpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/molgpt/test_10k/ --num_props 0 --max_epochs 10 --eval --num_to_sample 1000
 
 # MCMG
 # python MCMG/data_structs.py --input_file data/moses/data_10k.csv --output_dir data/moses/MCMG/
-# python MCMG/train_prior.py --train-data data/moses/data_10k.csv --valid-data data/moses/data_10k.csv --voc_path data/moses/MCMG/Voc_RE --output_dir result/MCMG/test_10k/ --num_epochs 1
-
-# MolGPT
-# python molgpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/molgpt/test_10k/ --num_props 0 --max_epochs 30 --eval
-
-# GPT
-# python gpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/gpt/test_10k/ --eval --n_epochs 30
+# python MCMG/train_prior.py --train-data data/moses/data_10k.csv --valid-data data/moses/data_10k.csv --voc_path data/moses/MCMG/Voc_RE --output_dir result/MCMG/test_10k/ --num_epochs 10 --eval --num_to_sample 1000
 
 # SGPT
-# python sgpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/charRNN/test_10k/ --eval --n_epochs 30
+# python sgpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/charRNN/test_10k/ --eval --n_epochs 10 --num_to_sample 1000 --n_embd 256 --n_head 16 --n_layers 8 --batch_size 512
 
-# SmilesRNN
-python SmilesRNN/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/SmilesRNN/test_10k/ --eval --n_epochs 30
+# GPT
+python gpt/train.py --train_data data/moses/data_10k.csv --valid_data data/moses/data_10k.csv --output_dir result/gpt/test_10k/ --eval --n_epochs 10 --num_to_sample 1000 --n_embd 256 --n_head 16 --n_layers 8 --batch_size 512
